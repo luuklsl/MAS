d88b96203d7060bf564be4b7552a32b32d4327c6
Luuk Lenders <luuklsll@gmail.com>
2017-10-09 11:32:24 +0200

    Init

---
+++ sytem.py
@@ -0,0 +1,29 @@
+"""
+Roads: connect one node to th other, roads have a travel time defined as  "C1+T/C2" where C1 and C2 are two constants
+Node: a point where cars can change the road they are on
+Car: one 'entitiy' that can be on a road, is considered the agent in our system. 
+	Cars can be set to either A* to "Optimal road sharing"
+"""
+
+
+
+def setup():
+	N_nodes = 4
+
+
+	pass
+
+
+def roads(list, time):
+	"""We expect a road to be one way (list[a:b], time, and amount of cars on it (?)"""
+	begining = list[0]
+	end = list[1]
+	#time can be used directly
+
+def cars():
+	pass
+
+def nodes(int):
+	pass
+
+	
